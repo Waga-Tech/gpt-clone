f9189f84db39887bba55f770671eaf58c32da217
Waga-Tech <waganeshadmase@gmail.com>
2026-05-30 20:26:06 +0300

    feat: initialize frontend with React, Vite, and essential components
    - Add package.json with dependencies and scripts for development
    - Include favicon and SVG icons for UI
    - Create App component with chat functionality and API integration
    - Implement ChatHeader, ChatInput, MessageList, and ChatMessage components
    - Style components with CSS modules for a cohesive design
    - Set up Vite configuration for React project

---
+++ backend/db/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS conversations (
+  id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  role ENUM('user', 'assistant') NOT null,
+  content TEXT NOT null,
+  token_count INT UNSIGNED NOT NULL DEFAULT 0,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
---
+++ backend/index.js
@@ -0,0 +1,50 @@
+import "dotenv/config";
+
+import express from "express"; //using ES6 module standard not Common js
+import db from "./db/db.config.js";
+
+import mainRouter from "./src/api/main.routes.js";
+import cors from "cors"
+import { errorHandler } from "./src/middleware/error-handler.js";
+
+const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
+app.use(express.json()); //middleware to parse json data from request body
+
+app.use("/api", mainRouter);
+// final error handling middleware
+app.use(errorHandler);
+
+// app.post("api/chat/conversations", (req, res) => {
+//   res.send("post method");
+// });
+
+// app.get("api/chat/conversations", (req, res) => {
+//   res.send("get method");
+// });
+
+async function startServer() {
+  try {
+    const connection = await db.getConnection();
+    connection.release();
+    // console.log("db connected");
+
+    app.listen(3888, (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log("server is running on port http://localhost:3888");
+    });
+  } catch (error) {
+    console.error("error starting server:", error);
+  }
+}
+
+startServer();
+// app.listen(3888, () => {
+//   console.log("server is running on port http://localhost:30000");
+// });
