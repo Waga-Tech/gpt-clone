17994861853260cf99d23725128a7e90000fc9fa
Waga-Tech <waganeshadmase@gmail.com>
2026-05-30 21:11:27 +0300

    fix: correct user property and add SSL configuration in database pool

--- backend/db/db.config.js
+++ backend/db/db.config.js
@@ -4,8 +4,12 @@ import mysql from "mysql2/promise";
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  user: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  ssl: {
+    ca: process.env.DB_CA_CERT,
+  },
 });
 
 export default db;
