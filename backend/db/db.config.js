2eea1db62f90cb0e7f17d0789ab541df03205237
Waga-Tech <waganeshadmase@gmail.com>
2026-05-30 22:26:50 +0300

    fix: correct database configuration by removing duplicate user property and adjusting port assignment

--- backend/db/db.config.js
+++ backend/db/db.config.js
@@ -1,10 +1,9 @@
 import mysql from "mysql2/promise";
-// import Pool from "./../node_modules/undici-types/pool.d";
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
-  user: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   ssl: {
@@ -13,3 +12,19 @@ const db = mysql.createPool({
 });
 
 export default db;
+
+// import mysql from "mysql2/promise";
+// // import Pool from "./../node_modules/undici-types/pool.d";
+
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   port: process.env.DB_PORT,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+//   ssl: {
+//     ca: process.env.DB_CA_CERT,
+//   },
+// });
+
+// export default db;
